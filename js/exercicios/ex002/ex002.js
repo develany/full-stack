372427e35b0fc36727fa8573179ed02806509f06
Elany Peixoto <elanysantos5@outlook.com>
2023-02-02 20:22:34 -0300

    ex02

--- js/exercicios/ex002/ex002.js
+++ js/exercicios/ex002/ex002.js
@@ -1,7 +1,9 @@
 // Crie uma função que retorne o seu nome completo, ela deve possuir os parâmetros: nome e sobrenome
 function nomeCompleto(nome, sobrenome){
-    const nomeCompleto = 
+    var nomeCompleto = nome + sobrenome
 }
-const lado = +prompt('digite o valor da aresta do quadrado')
-const retorno = perimetro(lado)
-alert (retorno)
+var nome= document.querySelector('input#nome')
+var sobrenome= document.querySelector('input#sobrenome')
+const retorno = `Seu nome completo é ${nomeCompleto}!`
+var resultado = document.querySelector('div#retorno')
+resultado = retorno.innerHTML(`<p> Seu nome é ${nomeCompleto}!`)
