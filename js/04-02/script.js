eb7a27c421521d872d937fb39de074a6cce2ef6f
Elany Peixoto <elanysantos5@outlook.com>
2023-02-04 11:03:08 -0300

    aula

---
+++ js/04-02/script.js
@@ -0,0 +1,21 @@
+//document.body.innerText = 'Bom dia'
+const saudacao = document.getElementById('saudacao')
+
+const horaMomento = new Date().getHours()
+//const nomeUsuario = ''
+if(horaMomento<12){
+    saudacao.innerHTML = 'Bom dia'
+}else if (horaMomento>=18){
+    saudacao.innerHTML = 'Boa noite'
+}else{
+    saudacao.innerHTML = 'Boa tarde'
+}
+///const lista = ['Ivan', ' Ari', 'Ranel', 'Ari' ]
+//for (let i=0; i < lista.length; i++){
+ //   console.log(lista [i])
+//}
+let i = 0
+while(i<10){
+    console.log(i)
+    i++
+}
