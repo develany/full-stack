6587e630efa2eae634dc19300f7618564a323131
Elany Peixoto <elanysantos5@outlook.com>
2023-02-04 12:10:48 -0300

    Update script.js

--- js/04-02/script.js
+++ js/04-02/script.js
@@ -10,12 +10,22 @@ if(horaMomento<12){
 }else{
     saudacao.innerHTML = 'Boa tarde'
 }
-///const lista = ['Ivan', ' Ari', 'Ranel', 'Ari' ]
+const lista = ['Ivan', ' Ari', 'Ranel', 'Ari' ]
+
 //for (let i=0; i < lista.length; i++){
  //   console.log(lista [i])
 //}
-let i = 0
-while(i<10){
-    console.log(i)
-    i++
+//let i = 0
+//while(i<5){
+//    console.log(i)
+ //   i=i+2
+//}
+//let respota
+//do{
+ //   respota = confirm('Vai me passa im pix?')
+//}while(!respota)
+
+function concaten1 (texto, index){
+    alert(index + '-' + texto)
 }
+lista.forEach(concaten1)
