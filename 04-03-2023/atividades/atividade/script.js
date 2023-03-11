1bc4661b3c8868b38703b4824c653c52b60e3d41
Elany Peixoto <elanysantos5@outlook.com>
2023-03-11 10:23:53 -0300

    update

---
+++ 04-03-2023/atividades/atividade/script.js
@@ -0,0 +1,5 @@
+const texto = 'Bom dia!'
+const nome = 'Elany'
+let saudacao = document.getElementById('oi')
+
+saudacao.innerText = `${texto} ${nome}`
--- 04-03-2023/script.js
+++ 04-03-2023/script.js
@@ -3,16 +3,16 @@
 
 //const texto = 'elane silva'
 //alert(texto.toUpperCase()) // maiusculas
-//alert(texto.toLocaleLowerCase()) //miniusculas
+//alert(texto.toLocaleLowerCase()) //minusculas
 //alert(texto.slice(0,3)) // escolhe o que mostrar
 //alert(texto.length) //quantos caracteres
-//alert(texto.charAt(0))
+//alert(texto.charAt(0))//pega o elmento especifico
 //alert(texto.slice(0,1).toLocaleUpperCase() + texto.slice(1))
 //.join('') junta e transforma em texto
 //console.log(texto.split(' ')) //separa e transforma em array
 //alert(texto.replace(/e/g, 'E')) // substitui g globaliza
 //alert(texto.replaceAll(e, 'E')) // substitui tudo
-
+//.trim() // tira os espaços do inicio e do fim
 
 // ---------
 
