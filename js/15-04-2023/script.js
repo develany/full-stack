b85029adee6dec8f1c74c9d494a843c38725a69c
Elany Peixoto <elanysantos5@outlook.com>
2023-04-23 20:40:13 -0300

    Update script.js

--- js/15-04-2023/script.js
+++ js/15-04-2023/script.js
@@ -1,3 +1,28 @@
- async function deleteById(id){
+function dtoUser(user) {
 
+    return { nome: user.first_name, email: user.email }
+}
+
+async function buscarTodos() {
+
+    try {
+        const response = await fetch('https://reqres.in/api/users')
+
+        // const { data: users } = await response.json()
+
+        const pagina = await response.json()
+
+        console.log(pagina.data)
+
+
+    } catch (error) {
+        console.error(error.message)
+    }
+
+}
+
+buscarTodos()
+
+async function deleteById(id) {
+    fetch('https://reqres.in/api/users')
 }
